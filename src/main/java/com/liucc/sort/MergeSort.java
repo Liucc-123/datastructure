f93f03f7b9f9818843028af155a4ef4a7ae5826d
创创刘 <2514573000@qq.com>
2025-01-04 22:14:36 +0800

    add 归并排序

--- src/main/java/com/liucc/sort/MergeSort.java
+++ src/main/java/com/liucc/sort/MergeSort.java
@@ -8,7 +8,9 @@ import java.util.Arrays;
 public class MergeSort {
     public static void main(String[] args) {
         int[] arr = {5, 3, 8, 4, 7};
-        mergeSort(arr, 0, arr.length-1);
+        // 优化：将临时数组的定义放到外面，避免每次递归调用都创建销毁临时数组，节省内存开销
+        int[] temp = new int[arr.length];
+        mergeSort(arr, 0, arr.length-1, temp);
         System.out.println("排序后的数组：" + Arrays.toString( arr));
     }
 
@@ -19,7 +21,7 @@ public class MergeSort {
      * @param left
      * @param right
      */
-    public static void mergeSort(int[] arr, int left, int right){
+    public static void mergeSort(int[] arr, int left, int right, int[] temp){
         // 打印分治过程，看看数组如何被拆分的
 //        System.out.println(Arrays.toString(Arrays.copyOfRange(arr, left, right + 1)));
         // 2、治理：子序列左右指针相等，说明子序列只有一个元素，无需再分解
@@ -28,22 +30,20 @@ public class MergeSort {
         }
         // 1、分解
         int mid = (left + right) / 2;
-        mergeSort(arr, left, mid); // 分解左半部分
-        mergeSort(arr, mid + 1, right); // 分解右半部分
+        mergeSort(arr, left, mid, temp); // 分解左半部分
+        mergeSort(arr, mid + 1, right, temp); // 分解右半部分
         // 3、合并
-        merge(arr, left, right, mid);
+        merge(arr, left, right, mid, temp);
     }
 
     /**
      * 合并
      * @param arr 原始数组
-     * @param left
-     * @param right
-     * @param mid
+     * @param left 数组左边界
+     * @param right 数组右边界
+     * @param mid 中间切分位置
      */
-    private static void merge(int[] arr, int left, int right, int mid) {
-        // 定义临时数组temp
-        int[] temp = new int[right - left + 1];
+    private static void merge(int[] arr, int left, int right, int mid, int[] temp) {
         int i = left; // 左指针
         int j = mid + 1; // 右指针
         int k = 0;// 遍历temp数组
